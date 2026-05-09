9bb71e4b3e5866cd9df67e0d3cb607696d500997
truongnp <truongnp@fabatechnology.com>
2026-05-09 10:02:03 +0700

    first commit

---
+++ src/configs/app.ts
@@ -0,0 +1,11 @@
+export const app = {
+    webcomeMessage: "Welcome to Nờ Pê Tê",
+    name: "Nờ Pê Tê",
+    description: "Nờ Pê Tê - Dịch vụ thiết kế website chuyên nghiệp",
+    url: "https://nopete.com",
+    logo: "/logos/logo.png",
+    footer: {
+        copyright: "All right reserved",
+        author: "TruongNpt",
+    },
+}
